97e4af6e0fe33312dd22372a640b852c812689f0
thefamilyman400 <thefamilyman400@gmail.com>
2026-05-06 20:10:43 +0530

    Create hello.py

---
+++ scripts/hello.py
@@ -0,0 +1,7 @@
+import datetime, platform, os
+print("==============================")
+print(" Hello from Python Script!")
+print(f" Python version: {platform.python_version()}")
+print(f" Run time: {datetime.datetime.now()}")
+print(f" Directory: {os.getcwd()}")
+print("==============================")
